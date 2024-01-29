99efc03835d340d12bd56f46de5202b41374eba0
Jayaprakash J <jayaprakash@whydigit.com>
2024-01-29 23:34:35 +0530

    cross config changes

--- src/main/java/com/whydigit/efit/config/CorsConfig.java
+++ src/main/java/com/whydigit/efit/config/CorsConfig.java
@@ -1,19 +1,21 @@
 package com.whydigit.efit.config;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    private static final long MAX_AGE_SECONDS = 3600;
+	private static final long MAX_AGE_SECONDS = 3600;
+	
+	@Value("${cors.allowed-origins}")
+	private String[] allowedOrigins;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")  // Allow all origins
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECONDS);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins(allowedOrigins)
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
+				.allowCredentials(true).maxAge(MAX_AGE_SECONDS);
+	}
 }
