8ea86d72555ab3ea32cd75c12b2542e99fd60cab
Jayaprakash J <jayaprakash@whydigit.com>
2024-01-29 19:10:44 +0530

    cross config changes

--- src/main/java/com/whydigit/efit/config/CorsConfig.java
+++ src/main/java/com/whydigit/efit/config/CorsConfig.java
@@ -1,34 +1,21 @@
 package com.whydigit.efit.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
+public class CorsConfig implements WebMvcConfigurer {
 	private static final long MAX_AGE_SECONDS = 3600;
 
 	@Value("${cors.allowed-origins}")
 	private String[] allowedOrigins;
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList(allowedOrigins));
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("DELETE");
-		config.setMaxAge(MAX_AGE_SECONDS);
-		config.addAllowedHeader("*");
-		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins(allowedOrigins)
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
+				.allowCredentials(true).maxAge(MAX_AGE_SECONDS);
 	}
 }
